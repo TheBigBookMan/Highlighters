45135c14d4c50cc3b0c1574da24260ed4b5e3b94
Ben Smerd <bsmerd@adam.com.au>
2023-03-17 21:51:02 +1030

    add in nav bar and links to pages

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav</h1>
+    <div className="p-4 flex justify-between items-center w-auto">
+      <Link href="/">
+        <h1>Highlighters</h1>
+      </Link>
+      <ul className="flex items-center gap-4">
+        <li>
+          <Link href="/profile">
+            <p>Profile</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/newsfeed">
+            <p>Newsfeed</p>
+          </Link>
+        </li>
+        <li className="bg-teal-500 py-2 px-4 rounded-xl text-white">
+          <Link href="/auth">
+            <p>Sign In</p>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
