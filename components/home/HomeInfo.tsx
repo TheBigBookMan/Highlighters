da00f3a4be21bda3ff951494aa2c471740e44fd2
Ben Smerd <bsmerd@adam.com.au>
2023-03-18 14:17:17 +1030

    added in the homepageinfo component

--- components/home/HomeInfo.tsx
+++ components/home/HomeInfo.tsx
@@ -1,10 +1,17 @@
 "use client";
 const HomeInfo = () => {
   return (
-    <div className="p-4 flex flex-col justify-center items-center">
+    <div className="p-20 flex flex-col justify-center items-center">
       <div className="mx-auto">
         <h1 className="font-lobster font-bold text-4xl">Highlighters</h1>
-        <h1>Highlighters is a social media app</h1>
+        <h1>
+          Highlighters is a social media app where users upload a moment from
+          their life that they find was a highlight for that time period. For
+          example: if you went on a nice hike one week which you found really
+          made your mood better, you can upload an image of that hike with a
+          description. You can then look back on all the highlights you have had
+          from each week and remember how good life can be!
+        </h1>
       </div>
     </div>
   );
