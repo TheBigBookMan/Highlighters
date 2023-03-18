fae9805c33a179397eb74046c9d68108d5f1396b
Ben Smerd <bsmerd@adam.com.au>
2023-03-19 09:46:33 +1030

    added in my posts timeframe selector

--- components/profile/MyPosts.tsx
+++ components/profile/MyPosts.tsx
@@ -1,10 +1,27 @@
+"use client";
+
 const hardcode = ["Daily", "Weekly", "Monthly", "Yearly"];
+import { useState, useEffect } from "react";
 
 const MyPosts = () => {
+  const [timeframe, setTimeframe] = useState<string>("Daily");
+
   return (
-    <div className="shadow-xl rounded-lg h-full w-full p-4 flex">
+    <div className="shadow-xl rounded-lg h-full w-full p-4 flex flex-col">
       <h1 className="font-bold text-2xl text-teal-500">My Posts</h1>
-      <ul className="flex gap-2"></ul>
+      <ul className="flex gap-2">
+        {hardcode.map((time) => (
+          <li
+            onClick={() => setTimeframe(time)}
+            key={time}
+            className={`${
+              timeframe === time && "font-bold text-teal-500"
+            } cursor-pointer`}
+          >
+            {time}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
