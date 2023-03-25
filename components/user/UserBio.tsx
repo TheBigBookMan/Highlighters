8feca1128d68cf3a5f4ad1a504c476437a89814a
Ben Smerd <bsmerd@adam.com.au>
2023-03-25 14:37:19 +1030

    add in follow/unfollow button for users

--- components/user/UserBio.tsx
+++ components/user/UserBio.tsx
@@ -2,13 +2,13 @@
 import { db } from "@/utils/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
+import { SlUserFollow, SlUserUnfollow } from "react-icons/sl";
 
 // !!! fix params shouldnt be null on global for params
 const UserBio = ({ params }: Params) => {
   const userId = params.user;
   const [userInfo, setUserInfo] = useState<User | undefined>();
 
-  console.log(userId);
   const getData = async () => {
     try {
       const docRef = doc(db, "users", userId);
@@ -28,6 +28,8 @@ const UserBio = ({ params }: Params) => {
     }
   }, []);
 
+  let follows = false;
+
   console.log(userInfo);
   return (
     <div className="flex gap-4 md:px-16 h-[140px] w-full shadow-xl rounded-xl p-2">
@@ -39,8 +41,22 @@ const UserBio = ({ params }: Params) => {
       <div className="flex flex-col w-full max-w-[400px]">
         <div className="flex justify-between items-center pr-4">
           <div className="flex flex-col gap-1">
-            <h1 className="font-bold">{userInfo?.displayName}</h1>
-
+            <div className="flex gap-2 items-center">
+              <h1 className="font-bold text-teal-500">
+                {userInfo?.displayName}
+              </h1>
+              {follows ? (
+                <button className="flex gap-2 items-center bg-red-400 w-[120px] py-1 px-4 rounded-xl text-white hover:bg-red-600">
+                  <SlUserUnfollow />
+                  Unfollow
+                </button>
+              ) : (
+                <button className="flex gap-2 items-center bg-teal-500 w-[120px] py-1 px-4 rounded-xl text-white hover:bg-teal-600">
+                  <SlUserFollow />
+                  Follow
+                </button>
+              )}
+            </div>
             <p
               className={`overflow-x-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-track-rounded scrollbar-thumb-teal-500 scrollbar-track-gray-200`}
             >
