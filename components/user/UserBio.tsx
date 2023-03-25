fd90ba713ea47e77d2f8500df44e6e7ac5d6b734
Ben Smerd <bsmerd@adam.com.au>
2023-03-25 12:39:22 +1030

    add in layour for the users profile page bio

--- components/user/UserBio.tsx
+++ components/user/UserBio.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { db } from "@/utils/firebase";
-import { doc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 
 // !!! fix params shouldnt be null on global for params
@@ -12,6 +12,11 @@ const UserBio = ({ params }: Params) => {
   const getData = async () => {
     try {
       const docRef = doc(db, "users", userId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap) {
+        const docData = docSnap.data();
+        setUserInfo({ ...docData });
+      }
     } catch (err) {
       console.log(err);
     }
@@ -23,10 +28,27 @@ const UserBio = ({ params }: Params) => {
     }
   }, []);
 
-  console.log(params);
+  console.log(userInfo);
   return (
     <div className="flex gap-4 md:px-16 h-[140px] w-full shadow-xl rounded-xl p-2">
-      <h1>Person</h1>
+      <img
+        src={userInfo?.image}
+        alt={userInfo?.displayName}
+        className="w-20 h-20 rounded-full"
+      />
+      <div className="flex flex-col w-full max-w-[400px]">
+        <div className="flex justify-between items-center pr-4">
+          <div className="flex flex-col gap-1">
+            <h1 className="font-bold">{userInfo?.displayName}</h1>
+
+            <p
+              className={`overflow-x-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-track-rounded scrollbar-thumb-teal-500 scrollbar-track-gray-200`}
+            >
+              {userInfo?.description}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
