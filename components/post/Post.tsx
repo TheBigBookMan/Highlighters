1d11a0b9daf7cadd72d785c4cdcb1cbd0e73f8b6
Ben Smerd <bsmerd@adam.com.au>
2023-03-26 09:10:27 +1030

    add in unlike button for users on posts

--- components/post/Post.tsx
+++ components/post/Post.tsx
@@ -69,6 +69,21 @@ const Post = ({ params }: Params) => {
       console.log(err);
     }
   };
+
+  const unlikeButton = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = doc(db, "posts", postId);
+      const currentLikes = postData?.likedByUsers;
+      const indexOfId = currentLikes?.indexOf(loggedInUser?.id);
+      if (!indexOfId) return;
+      currentLikes?.splice(indexOfId, 1);
+      const updatedDoc = { ...postData, likedByUsers: [...currentLikes] };
+      await updateDoc(docRef, updatedDoc);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   console.log(postData);
 
   useEffect(() => {
@@ -80,34 +95,50 @@ const Post = ({ params }: Params) => {
 
   return (
     <div className="flex flex-col max-w-[600px]  gap-4 shadow-xl p-2 rounded-lg">
-      <div className="flex gap-2 items-center">
-        <p>{postData?.timeframe} Highlight:</p>
-        <h1 className="font-bold text-xl text-teal-500">{postData?.title}</h1>
-      </div>
-      <p>Posted by: {postData?.userName}</p>
-      <div className="flex gap-6">
-        <p>{postData?.date}</p>
-        <div className="flex gap-2">
-          <div className="flex gap-1 items-center">
-            <SlSpeech className="text-xl" />
-            <p>{postData?.comments}</p>
+      {!loggedInUser ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <div className="flex gap-2 items-center">
+            <p>{postData?.timeframe} Highlight:</p>
+            <h1 className="font-bold text-xl text-teal-500">
+              {postData?.title}
+            </h1>
           </div>
-          <div className="flex gap-1 items-center">
-            <FiThumbsUp
-              onClick={likeButton}
-              className="text-lg cursor-pointer hover:text-teal-500"
-            />
-            <p>{postData?.likes}</p>
-          </div>
-        </div>
-      </div>
-      <p>{postData?.location}</p>
+          <p>Posted by: {postData?.userName}</p>
+          <div className="flex gap-6">
+            <p>{postData?.date}</p>
+            <div className="flex gap-2">
+              <div className="flex gap-1 items-center">
+                <SlSpeech className="text-xl" />
+                <p>{postData?.comments}</p>
+              </div>
+              <div className="flex gap-1 items-center">
+                {postData?.likedByUsers.includes(loggedInUser?.id) ? (
+                  <FiThumbsUp
+                    onClick={unlikeButton}
+                    className="text-lg cursor-pointer text-teal-500 hover:text-black"
+                  />
+                ) : (
+                  <FiThumbsUp
+                    onClick={likeButton}
+                    className="text-lg cursor-pointer hover:text-teal-500"
+                  />
+                )}
 
-      <img
-        src={postData?.image}
-        className="w-full max-w-[700px] rounded-xl shadow-xl"
-      />
-      <p>{postData?.description}</p>
+                <p>{postData?.likedByUsers.length}</p>
+              </div>
+            </div>
+          </div>
+          <p>{postData?.location}</p>
+
+          <img
+            src={postData?.image}
+            className="w-full max-w-[700px] rounded-xl shadow-xl"
+          />
+          <p>{postData?.description}</p>
+        </>
+      )}
     </div>
   );
 };
