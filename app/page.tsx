5f1d6c4a86e4c92a4cab13329b9e1fecaee74bb9
Ben Smerd <bsmerd@adam.com.au>
2023-04-07 16:34:31 +0930

    update intro html

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
           your life that you find was a highlight for that time period. For
           example: if you went on a nice hike one day which you found really
           made your mood better, you can upload an image of that hike with a
-          description to the "daily" highlight timeframe. At the end of the week
-          you can look at the highlights you did for each "daily" of that week
-          and then choose which one you want to be your "weekly" highlight. You
-          then repeat this for each of the "weekly" highlights to choose your
-          "monthly" and then "monthly" for your "yearly". This makes you think
-          about all the good highlights you have had in the previous time
-          periods. You can then look back on all the highlights you have had
-          from each timeframe and remember how good life can be!
+          description to the daily highlight timeframe. At the end of the week
+          you can look at the highlights you did for each daily of that week and
+          then choose which one you want to be your weekly highlight. You then
+          repeat this for each of the weekly highlights to choose your monthly
+          and then monthly for your yearly. This makes you think about all the
+          good highlights you have had in the previous time periods. You can
+          then look back on all the highlights you have had from each timeframe
+          and remember how good life can be!
         </p>
       </div>
     </main>
